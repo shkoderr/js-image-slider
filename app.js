c119c3a189b6881a78187f16a457e8b8e6c4d12a
Timur <elecktrohachyck@gmail.com>
2023-02-16 15:48:52 +0400

    added a function activeSlide

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-//Получим наши слайды, создав переменную
+//Добавим программно активный класс элементу создав плагин:
+function slidesPlugin(activeSlide = 2) {   //по умолчанию будет активен элемент под индексом 4
+  //Получим наши слайды, создав переменную
 const slides = document.querySelectorAll('.slide')
 
+slides[activeSlide].classList.add('active')   //выбранному под индексом элементу массива slides придаём класс 'active'
 //Обойдём циклом по каждому слайду и добавим слушателя
 //способ for of
 for (const slide of slides) {
@@ -15,3 +18,6 @@ function clearActiveClasses() {
     slide.classList.remove('active')      //на каждой итерации убираем активный класс у всех слайдов
   })
 }
+}
+//Вызовем нашу функцию, чтобы придать активный класс слайду: 
+slidesPlugin()
